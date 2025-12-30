1bc96cb54fe1ced44a9def0052e6552ff668c692
Guilherme Monni <gmonni20@gmail.com>
2025-12-30 20:33:46 -0300

    initial idea

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
 
     return(
-        <footer className="w-full bg-deep text-gray-400 text-center py-6 text-sm mt-full">
+        <footer className="w-full bg-violet text-center py-6 text-sm mt-full">
             © 2025 KindlyHelp • Gravataí, RS • gmonni002@gmail.com
         </footer>
     )
