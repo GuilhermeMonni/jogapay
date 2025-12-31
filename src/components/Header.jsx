464f65e011047bcbd0b0874a87f964ec8e7334fa
Guilherme Monni <gmonni20@gmail.com>
2025-12-30 21:31:25 -0300

    style initial header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 function Header() {
 
     return(
-    <div className="bg-blue-200 w-full h-36 p-4 flex">
-        <img src="https://res.cloudinary.com/dzbdewkbp/image/upload/v1767051401/banco-central_1_lkoob2.png" alt="Icone banco" className="my-auto w-20 h-20"/>   
-        <h1 className="">JogaPay</h1> 
-        <p>Pagamentos, rendimentos e negociações</p>
-        {/* colocar um contador de qtd de jogadores */}
+    <div className="bg-primary w-full h-36 p-2 flex">
+        <img src="https://res.cloudinary.com/dzbdewkbp/image/upload/v1767051401/banco-central_1_lkoob2.png" alt="Icone banco" className="mt-4 w-20 h-20 justify-center"/>   
+        <span className="p-1 w-2/4 mx-3">
+            <h1 className="font-cherry text-3xl mt-5">JogaPay</h1>
+            <p className="font-varela font-medium text-base mt-2 text-subtitle hover:text-black">Pagamentos, rendimentos e negociações</p>
+        </span>
+        {/* colocar um contador de qtd de jogadores 
+            PAREI NA INSTALAÇÃO DO MAGIC.CSS
+        */}
     </div> 
     )
 }
